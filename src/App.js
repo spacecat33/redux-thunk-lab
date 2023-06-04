75bd37490485a93c57a338e548f6538fa848f046
Janan Akkad <jananakkad@hotmail.co.uk>
2023-06-04 21:03:28 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap'
 import { connect } from "react-redux";
 import { fetchCats } from "./actions/catActions";
 import CatList from "./CatList"
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import catsReducer from './cats_reducer'
-
-export default combineReducers({
-  cats: catsReducer
-});
